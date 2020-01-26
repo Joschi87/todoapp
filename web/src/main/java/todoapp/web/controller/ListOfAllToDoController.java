f330b92368e4e60cf2619588d58eeba7740f27f2
Joschi87 <janosch.walter27@gmail.com>
2020-01-26 12:24:14 +0100

    Reset to a working version by return the content of the database

--- web/src/main/java/todoapp/web/controller/ListOfAllToDoController.java
+++ web/src/main/java/todoapp/web/controller/ListOfAllToDoController.java
@@ -1,14 +1,14 @@
 package todoapp.web.controller;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import todoapp.web.entity.ListOfAllToDoEntity;
 import todoapp.web.service.ListOfAllToDosService;
 
 @RestController
@@ -20,17 +20,11 @@ public class ListOfAllToDoController {
 	@RequestMapping(
 					path="/allToDoAsList",
 					method = RequestMethod.GET)
-	public String getAllToDosFromDatabase() {
+	public List<ListOfAllToDoEntity> getAllToDosFromDatabase() {
 		
-		JSONArray listArray = new JSONArray(listService.getAllToDos());
-		List<String> test = new ArrayList<>();
-		for(int i = 0; i < listArray.length(); i++) {
-			test.add(listArray.getJSONObject(i).getString("toDoDate"));
-		}
 		
-		System.out.println(test);
 		
-		return "test";
+		return listService.getAllToDos();
 		
 	}
 	
