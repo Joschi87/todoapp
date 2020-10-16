806dcb4185d2924aab5c38fd8fee9974c7be4a1b
Joschi87 <janosch.walter27@gmail.com>
2020-10-16 21:28:19 +0200

    add exceptions

---
+++ web/src/main/java/todoapp/web/lib/exception/CreateToDoException.java
@@ -0,0 +1,9 @@
+package todoapp.web.lib.exception;
+
+public class CreateToDoException extends Exception{
+
+	public CreateToDoException(String cause) {
+		super(cause);
+	}
+	
+}
---
+++ web/src/main/java/todoapp/web/lib/exception/LoginException.java
@@ -0,0 +1,9 @@
+package todoapp.web.lib.exception;
+
+public class LoginException extends Exception{
+	
+	public LoginException(String cause) {
+		super(cause);
+	}
+
+}
